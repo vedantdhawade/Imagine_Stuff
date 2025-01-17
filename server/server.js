0253d661c073217b73258a80ebecf6dafe183bfa
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-01-17 20:22:41 +0530

    Done

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 import express from "express";
-
+import axios from "axios";
+import fs from "fs";
 const app = express();
-const port = 3000;
+import { HfInference } from "@huggingface/inference";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const hfapikey = "";
+
+app.get("/node-api", async (req, res) => {
+  const inference = new HfInference(hfapikey);
+
+  const result = await inference.textClassification({
+    model: "cardiffnlp/twitter-roberta-base-sentiment-latest",
+    inputs: "The meeting went as scheduled",
+  });
+  const data = await inference.questionAnswering({
+    model: "deepset/roberta-base-squad2",
+    inputs: {
+      question: "What is the capital of France?",
+      context: "The capital of France is Paris.",
+    },
+  });
+  const convo = await inference.imageClassification({
+    data: fs.readFileSync("images1.jpeg"),
+    model: "google/vit-base-patch16-224",
+  });
+  console.log(result[0].label);
+  console.log(data);
+  console.log(convo);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log("Node.js server running on http://localhost:3000");
 });
